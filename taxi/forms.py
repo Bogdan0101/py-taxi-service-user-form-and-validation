c38ba289d38f7aadd29126d7db9312ca4861f36b
Bogdan0101 <kononovb777@gmail.com>
2025-09-06 17:51:50 +0300

    Solution#2

--- taxi/forms.py
+++ taxi/forms.py
@@ -2,11 +2,10 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 
-from taxi.models import Driver, Car
 
-class DriverCreateForm(UserCreationForm):
+class DriverCreationForm(UserCreationForm):
     class Meta(UserCreationForm):
-        model = Driver
+        model = get_user_model()
         fields = "__all__"
 
     def clean_license_number(self):
@@ -15,15 +14,26 @@ class DriverCreateForm(UserCreationForm):
             raise forms.ValidationError("Invalid license number")
         if not license_number[5:].isdigit():
             raise forms.ValidationError("Invalid license number")
-        if not license_number[:3].isalpha():
+        if not license_number[:3].isupper():
             raise forms.ValidationError("Invalid license number")
         return license_number
 
 
-class DriverLicenseUpdateForm(DriverCreateForm):
+class DriverLicenseUpdateForm(forms.ModelForm):
     class Meta(UserCreationForm):
-        model = Driver
+        model = get_user_model()
         fields = ("license_number", )
+
+    def clean_license_number(self):
+        license_number = self.cleaned_data["license_number"]
+        if len(license_number) != 8:
+            raise forms.ValidationError("Invalid license number")
+        if not license_number[5:].isdigit():
+            raise forms.ValidationError("Invalid license number")
+        if not license_number[:3].isupper():
+            raise forms.ValidationError("Invalid license number")
+        return license_number
+
 
 class CarForm(forms.ModelForm):
     drivers = forms.ModelMultipleChoiceField(
@@ -33,5 +43,5 @@ class CarForm(forms.ModelForm):
     )
 
     class Meta:
-        model = Car
+        model = get_user_model()
         fields = "__all__"
--- taxi/views.py
+++ taxi/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .forms import DriverCreateForm, DriverLicenseUpdateForm, CarForm
+from .forms import DriverCreationForm, DriverLicenseUpdateForm, CarForm
 from .models import Driver, Car, Manufacturer
 
 
@@ -81,25 +82,30 @@ class CarDeleteView(LoginRequiredMixin, generic.DeleteView):
 
 
 class DriverListView(LoginRequiredMixin, generic.ListView):
-    model = Driver
+    model = get_user_model()
     paginate_by = 5
 
 
 class DriverDetailView(LoginRequiredMixin, generic.DetailView):
-    model = Driver
+    model = get_user_model()
     queryset = Driver.objects.all().prefetch_related("cars__manufacturer")
 
 
 class DriverCreateView(LoginRequiredMixin, generic.CreateView):
-    model = Driver
-    form_class = DriverCreateForm
+    model = get_user_model()
+    form_class = DriverCreationForm
     success_url = reverse_lazy("taxi:driver-list")
+    template_name = "taxi/driver_form.html"
 
 class DriverDeleteView(LoginRequiredMixin, generic.DeleteView):
-    model = Driver
+    model = get_user_model()
     success_url = reverse_lazy("taxi:driver-list")
+    template_name = "taxi/driver_confirm_delete.html"
 
 
 class DriverLicenseUpdateView(LoginRequiredMixin, generic.UpdateView):
-    model = Driver
+    model = get_user_model()
     form_class = DriverLicenseUpdateForm
+    template_name = "taxi/driver_license_update.html"
+    success_url = reverse_lazy("taxi:driver-list")
+
